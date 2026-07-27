dbddfa9b33964937ae97f645da505aaeb2025d5c
Krishna Sharma <krishnashwork21@gmail.com>
2026-07-27 17:18:33 +0530

    Initial

---
+++ src/pages/events/index.tsx
@@ -0,0 +1,22 @@
+const Page=()=>{
+    return (
+     <div>
+       <a href="">
+         <img />
+         <h2>Events in London</h2>
+        </a>
+     <br />
+        <a href="">
+         <img />
+         <h2>Events in San Francisco</h2>
+        </a>
+     <br />
+        <a href="">
+         <img />
+         <h2>Events in Barcelona</h2>
+         </a>
+       </div>
+    );
+}
+
+export default Page;
