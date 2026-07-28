b468b3e3d779c5d430cd4d1eb2f1f63cd5c8c6c6
Krishna Sharma <krishnashwork21@gmail.com>
2026-07-28 13:15:48 +0530

    Save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,14 @@ const Page=()=>{
       <a href="/aboutus">About Us</a>
       </nav>
 
-     <a href="">
+     <a href="/events/london">
       <img />
       <h2>Events in London</h2>
       <br />
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
      </a>
      <br />
-     <a href="">
+     <a href="/events/sanfran">
       <img />
       <h2>Events in San Francisco</h2>
       <br />
@@ -40,10 +40,12 @@ const Page=()=>{
 
 export default Page;
 
-export function getServerSideProps(){
+export async function getServerSideProps(){
+   const {events_categories}=await import("../../data/data.json");
+   console.log(events_categories);
    return{
       props:{
-         title:"Hello Everyone!"
+         data: events_categories
       },
    };
 }
