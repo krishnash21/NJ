c8089fff9770e4833a1441b3379412db028686a5
Krishna Sharma <krishnashwork21@gmail.com>
2026-07-29 16:33:12 +0530

    SG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
-const Page=({data})=>{
+import Image from "next/image";
+
+type EventCategory={
+   id:string;
+   title:string;
+   description:string;
+   image:string;
+};
+
+const Page=({data}:{data:EventCategory[] })=>{
   return(
     <div>
     <header>
      <nav style={{ display: 'flex', gap: '1.5rem', alignItems: 'center' }}> 
-      <img />
       <a href="/">Home</a>
       <a href="/events">Events</a>
       <a href="/aboutus">About Us</a>
@@ -11,7 +19,7 @@ const Page=({data})=>{
 
       {data.map((ev)=>(
          <a key={ev.id} href={`/events/${ev.id}`}>
-            <Image src={ev.image} /><h2>{ev.title}</h2><p>
+            <Image width={200} height={100} alt={ev.title} src={ev.image} /><h2>{ev.title}</h2><p>
                {ev.description}</p>
             </a>))}
      </header>
@@ -27,7 +35,6 @@ export default Page;
 
 export async function getServerSideProps(){
    const {events_categories}=await import("../../data/data.json");
-   console.log(events_categories);
    return{
       props:{
          data:events_categories,
